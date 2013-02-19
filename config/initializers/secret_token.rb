43c7248bc6fb5411a77a6939fed5e882cab9e545
Kit Pearson <dev_kit@icloud.com>
2013-02-19 14:41:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KitApp::Application.config.secret_token = '5d0a7ec07aec1e105e23b5031e757d94b4842c56fd440823e041908778add6d153aaa9e7426e23f9148b6076807eb3332bdbc11dbf00832c9655d0f580ec59f6'
